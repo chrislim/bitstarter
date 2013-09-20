f72d75229c86bf7fc1363af6611908abbbc8702c
Chris <kirisu@gmail.com>
2013-09-19 20:39:25 -0700

    adding improved checker script

--- grader.js
+++ grader.js
@@ -24,6 +24,7 @@ References:
 var fs = require('fs');
 var program = require('commander');
 var cheerio = require('cheerio');
+var restler = require('restler');
 var HTMLFILE_DEFAULT = "index.html";
 var CHECKSFILE_DEFAULT = "checks.json";
 
@@ -44,8 +45,7 @@ var loadChecks = function(checksfile){
 	return JSON.parse(fs.readFileSync(checksfile));
 };
 
-var checkHtmlFile = function(htmlfile, checksfile){
-	$ = cheerioHtmlFile(htmlfile);
+var checker = function($, checksfile){
 	var checks = loadChecks(checksfile).sort();
 	var out = {};
 	for(var ii in checks){
@@ -55,18 +55,46 @@ var checkHtmlFile = function(htmlfile, checksfile){
 	return out;
 };
 
+var checkHtmlFile = function(htmlfile, checksfile){
+	$ = cheerioHtmlFile(htmlfile);
+	return checker($, checksfile);
+};
+
+
+var checkHtmlUrl = function(url, checksfile, cb){
+	cheerioHtmlUrl(url, checksfile, function($, checksfile){
+		cb(checker($, checksfile));
+	});
+};
+
+var cheerioHtmlUrl = function(url, checksfile, cb){
+	console.log("fetching url");
+	var html = restler.get(url).on('success', function(data){
+		cb(cheerio.load(data), checksfile);
+	});
+};
+
 var clone = function(fn){
 	return fn.bind({});
+};
+
+var outputResult = function(checkJson){
+	var outJson = JSON.stringify(checkJson, null, 4);
+	console.log(outJson);
 };
 
 if(require.main == module){
 	program
 		.option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
 		.option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
+		.option('-u, --url <url>', 'URL to index.html')
 		.parse(process.argv);
-	var checkJson = checkHtmlFile(program.file, program.checks);
-	var outJson = JSON.stringify(checkJson, null, 4);
-	console.log(outJson);
+	if(program.url){
+		checkHtmlUrl(program.url, program.checks, outputResult);
+	} else {
+		var checkJson = checkHtmlFile(program.file, program.checks);
+		outputResult(checkJson);
+	}
 } else {
 	exports.checkHtmlFile = checkHtmlFile;
 }
